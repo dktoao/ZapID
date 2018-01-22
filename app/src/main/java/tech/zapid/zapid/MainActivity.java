b4cd7205679b787e55049d3803a14908ca8c68b6
Daniel Kuntz <daniel.kuntz@dk-cloud.net>
2018-01-21 21:55:55 -0700

    Add QR Code Reader

--- app/src/main/java/tech/zapid/zapid/MainActivity.java
+++ app/src/main/java/tech/zapid/zapid/MainActivity.java
@@ -1,18 +1,23 @@
 package tech.zapid.zapid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
 
     // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+    //static {
+    //    System.loadLibrary("native-lib");
+    //}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +28,23 @@ public class MainActivity extends AppCompatActivity {
 
     public void verify(View view) {
 
-        textView = findViewById(R.id.mainScreenText);
-        textView.setText("verify Called!");
+        IntentIntegrator scanner = new IntentIntegrator(this);
+        scanner.setBeepEnabled(false);
+        scanner.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
     public void addKeyWord(View view) {
@@ -38,9 +58,9 @@ public class MainActivity extends AppCompatActivity {
         textView.setText("addKeyCode Called!");
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    ///**
+    // * A native method that is implemented by the 'native-lib' native library,
+    // * which is packaged with this application.
+    // */
+    //public native String stringFromJNI();
 }
