b003d623f62b4a34f7ae129d5b053e32d72ac7b1
Daniel Kuntz <daniel.kuntz@dk-cloud.net>
2018-01-29 19:04:28 -0700

    Basic command line utility for making pass codes

--- zaputil/src/main/java/tech/zapid/zaputil/ZapIDPassGenerator.java
+++ zaputil/src/main/java/tech/zapid/zaputil/ZapIDPassGenerator.java
@@ -4,15 +4,19 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ZapIDPassGenerator {
+
+    private enum CsvFileSection {NA, GLOBAL, HEADER, INDIVIDUAL}
 
     public static void main(String[] args) {
 
         String inFile = args[0];
         String outFile = args[1];
-        String version = args[2];
+        int version = Integer.parseInt(args[2]);
 
         // Open the input file for reading
         BufferedReader in;
@@ -25,6 +29,9 @@ public class ZapIDPassGenerator {
         // Loop through and read each line
         String line = null;
         List<String> fields;
+        String globalInfo = null;
+        Map<Integer, String> localInfo = new HashMap<>();
+        CsvFileSection section = CsvFileSection.NA;
         while (true) {
             try {
                 line = in.readLine();
@@ -35,7 +42,71 @@ public class ZapIDPassGenerator {
                 x.printStackTrace();
             }
             fields = CSVUtils.parseLine(line);
-            System.out.printf("Ln0: %s, Ln1: %s\n", fields.get(0), fields.get(1));
+            // Find what section we are in
+            if ( fields.get(0).equals("GLOBAL")) {
+                section = CsvFileSection.GLOBAL;
+                continue;
+            } else if (fields.get(0).equals("INDIVIDUAL")) {
+                section = CsvFileSection.HEADER;
+                continue;
+            }
+            // Logic depending on the section
+            switch (section) {
+                case NA:
+                    break;
+                case GLOBAL:
+                    if (fields.get(0).charAt(0) == '_') {
+                        if (fields.get(0).charAt(1) == '_') {
+                            continue;
+                        } else {
+                            if (globalInfo == null) {
+                                globalInfo = fields.get(1);
+                            } else {
+                                globalInfo += ("\n" + fields.get(1));
+                            }
+                        }
+                    } else {
+                        if (globalInfo == null) {
+                            globalInfo = (fields.get(0)
+                                    + ": "
+                                    + fields.get(1));
+                        } else {
+                            globalInfo += ("\n"
+                                    + fields.get(0).substring(1)
+                                    + ": "
+                                    + fields.get(1));
+                        }
+                    }
+                    break;
+                case HEADER:
+                    for (int ii = 0; ii < fields.size(); ii++) {
+                        String field = fields.get(ii);
+                        if (field.charAt(0) == '_') {
+                            if (field.charAt(1) =='_') {
+                                continue;
+                            } else {
+                                localInfo.put(ii, "%s");
+                            }
+                        } else {
+                            localInfo.put(ii, (field + ": %s"));
+                        }
+                    }
+                    section = CsvFileSection.INDIVIDUAL;
+                    break;
+                case INDIVIDUAL:
+                    String message = globalInfo;
+                    for (int key: localInfo.keySet()) {
+                        if (message == null) {
+                            message = String.format(localInfo.get(key), fields.get(key));
+                        } else {
+                            message += ("\n" + String.format(localInfo.get(key), fields.get(key)));
+                        }
+                    }
+                    System.out.println(message);
+                    System.out.println(Util.byteToString(Util.getQRCode(message, version)));
+                    break;
+            }
+
         }
     }
 
